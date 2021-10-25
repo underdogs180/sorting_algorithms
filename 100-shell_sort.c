d16da3411ba5ddfff40d5487d9af66333a2b7ae6
underdogs180 <asadirshad38@gmail.com>
2021-10-25 10:04:36 -0700

    Fixed Error

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,27 +8,27 @@
  */
 void shell_sort(int *array, size_t size)
 {
-        int h = 1, in, out, tmp;
+int h = 1, in, out, tmp;
 
-        if (array == NULL || size < 2)
-                return;
-        while (h <= ((int)size / 3))
-                h = (h * 3) + 1;
+if (array == NULL || size < 2)
+return;
+while (h <= ((int)size / 3))
+h = (h * 3) + 1;
 
-        while (h > 0)
-        {
-                for (out = h; out < (int)size; out++)
-	        {
-	                tmp = array[out];
-	                in = out;
-	                while (in >= h && array[in - h] > tmp)
-	                { 
-	                        array[in] = array[in - h];
-	                        in = in - h;
-	                }
-	                array[in] = tmp;
-	        }
-                h = (h - 1) / 3;
-                print_array(array, size);
-         }
+while (h > 0)
+{
+for (out = h; out < (int)size; out++)
+{
+tmp = array[out];
+in = out;
+while (in >= h && array[in - h] > tmp)
+{
+array[in] = array[in - h];
+in = in - h;
+}
+array[in] = tmp;
+}
+h = (h - 1) / 3;
+print_array(array, size)
+}
 }
